f4884023eab30244ee3bf017fd879818224f976b
ShodiyorbekQambaraliyev <shodiyorbek057@gmail.com>
2025-04-11 17:46:00 +0500

    postga izox qismi qoshildii

--- LinkedIn/models.py
+++ LinkedIn/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Post(models.Model):
-    image = models.ImageField(upload_to='posts/')  # Rasm 'media/posts/' ichiga yuklanadi
+    image = models.ImageField(upload_to='posts/')
+    text = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)  # Post qachon yaratilganini saqlaydi
 
     def __str__(self):
